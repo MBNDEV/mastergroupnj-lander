bca99cd00c98654480eb6240a1061adbd65b6fc5
jundellagbo <jundellagbo10dd@gmail.com>
2025-10-21 20:16:43 +0800

    delay recaptcha

--- enhancer.php
+++ enhancer.php
@@ -76,7 +76,7 @@ function mbn_end_output_buffering($html) {
 
 
   // Define the substrings/needles to search for in scripts
-  $needles = array('gtag', 'gtms.js', 'googletagmanager');
+  $needles = array('gtag', 'gtms.js', 'googletagmanager', 'recaptcha');
 
   // Callback to process <script> tags
   $html = preg_replace_callback(
